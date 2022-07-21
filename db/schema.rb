f3de2762185111044e96186ce13c8fb0768b1461
tatsun777 <hakunan12@yahoo.co.jp>
2022-07-22 00:15:43 +0900

    login

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_21_092352) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_21_151137) do
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "image_name"
+    t.string "password"
   end
 
 end
