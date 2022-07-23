f4192b97e83cd90550e49cea01b40d21c462bdbd
tatsun777 <hakunan12@yahoo.co.jp>
2022-07-24 00:16:08 +0900

    like

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get "login" => "users#login_form"
-  post "login" => "users#login"
-  post "logout" => "users#logout"
+  post "likes/:post_id/create" => "likes#create"
+  post "likes/:post_id/destroy" => "likes#destroy"
 
   post "users/:id/update" => "users#update"
   get "users/:id/edit" => "users#edit"
@@ -9,6 +8,10 @@ Rails.application.routes.draw do
   get "signup" => "users#new"
   get "users/index" => "users#index"
   get "users/:id" => "users#show"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  get "login" => "users#login_form"
+  get "users/:id/likes" => "users#likes"
 
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
@@ -17,7 +20,7 @@ Rails.application.routes.draw do
   get "posts/:id/edit" => "posts#edit"
   post "posts/:id/update" => "posts#update"
   post "posts/:id/destroy" => "posts#destroy"
-  
+
   get "/" => "home#top"
   get "about" => "home#about"
 end
---
+++ db/migrate/20220723133611_add_user_id_to_likes2.rb
@@ -0,0 +1,5 @@
+class AddUserIdToLikes2 < ActiveRecord::Migration[7.0]
+  def change
+    add_column :likes,:post_id,:integer
+  end
+end
