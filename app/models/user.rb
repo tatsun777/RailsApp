805186e8c52bb55223258a171108452caf6d9b6d
tatsun777 <hakunan12@yahoo.co.jp>
2022-07-23 16:31:14 +0900

    add to post user_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
-    validates :password,{presence:true}
-end
+    validates :password, {presence: true}
+    
+    def posts
+      return Post.where(user_id: self.id)
+    end
+    
+  end
+  
